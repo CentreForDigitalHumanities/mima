38b6f9d4e95fa72033f041e9748279d6befe300b
Sheean Spoel <s.j.j.spoel@uu.nl>
2022-10-14 13:35:49 +0200

    Mapping from CSV to model

--- backend/upload/models.py
+++ backend/upload/models.py
@@ -1,9 +1,62 @@
 from distutils.command.upload import upload
 from django.db import models
-import os
+from typing import Callable, Dict, List, Sequence, Tuple
+
+def str_map(x: str):
+    return x
+
+def label_map(x: str):
+    return x.split('+')
+
+adverbial_mapping: Dict[str, Tuple[str, Callable[[str], any]]] = {
+    'ID': ('id', str_map),
+    'Manner adverbial': ('text', str_map),
+    'Full example': ('example', str_map),
+    'Translation': ('translation', str_map),
+    'Leipzig gloss': ('gloss', str_map),
+    'Language': ('language', str_map),
+    'Dialect': ('dialect', str_map),
+    'Language family': ('language_family', str_map),
+    'Language group': ('language_group', str_map),
+    'Source': ('source', str_map),
+    'Label': ('labels', label_map),
+    'Note(s)': ('notes', str_map)
+}
 
 class UploadedCSV(models.Model):
     upload_path = "backend/upload/csv_files"
     name = models.CharField(max_length=200)
-    file = models.FileField(upload_to=os.path.join(upload_path, name))
+    # file = models.FileField(upload_to=os.path.join(upload_path, name))
 
+class Adverbial:
+    @staticmethod
+    def validate_fieldnames(fieldnames: Sequence[str]) -> List[str]:
+        errors: List[List[str]] = []
+        missing_columns = list(adverbial_mapping.keys())
+        for name in fieldnames:
+            if name not in adverbial_mapping:
+                errors.append(['UNKNOWN_COLUMN', name])
+            else:
+                missing_columns.remove(name)
+
+        for name in missing_columns:
+            errors.append(['MISSING_COLUMN', name])
+
+        return errors
+
+
+    @staticmethod
+    def from_csv_row(row: Dict[str, str]) -> 'Adverbial':
+        adverbial = Adverbial()
+        for key, value in row.items():
+            name, mapping = adverbial_mapping[key]
+            setattr(adverbial, name, mapping(value))
+
+        return adverbial
+
+    def as_dict(self) -> Dict[str, any]:
+        result: Dict[str, any] = {}
+        for _, (key, _) in adverbial_mapping.items():
+            result[key] = getattr(self, key)
+
+        return result
--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Adverbial } from '../models/adverbial';
+import { ValidationErrors } from '../models/validationError';
 import { FileUploadService } from './../services/file-upload.service';
 
 @Component({
@@ -11,6 +12,7 @@ export class UploadComponent {
     adverbials: Adverbial[];
 
     loading = false;
+    messages: string[];
     file: File = null;
     shortLink = '';
 
@@ -21,19 +23,19 @@ export class UploadComponent {
         this.file = (event.target as HTMLInputElement).files[0];
     }
 
-    onUpload(): void {
+    async onUpload(): Promise<void> {
         this.loading = !this.loading;
-        console.log(this.file);
-
-        this.fileUploadService.upload(this.file).subscribe(
-            (adverbials: Adverbial[]) => {
-                this.loading = false;
-                // TODO: move this to service
-                for (const adverbial of adverbials) {
-                    adverbial['Labels'] = adverbial['Label'].split('+');
-                }
-                this.adverbials = adverbials;
+        delete this.messages;
+        try {
+            this.adverbials = await this.fileUploadService.upload(this.file);
+        } catch (error) {
+            if (error instanceof ValidationErrors) {
+                this.messages = Array.from(error.toStrings());
+            } else {
+                throw error;
             }
-        );
+        } finally {
+            this.loading = false;
+        }
     }
 }
