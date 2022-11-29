7b32e7cfff68391360ee551bf201bcf7c88f9b8c
Sheean Spoel <s.j.j.spoel@uu.nl>
2022-11-29 10:18:23 +0100

    Fixed test

--- frontend/src/app/services/language.service.spec.ts
+++ frontend/src/app/services/language.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { LanguageService } from './language.service';
@@ -6,7 +7,9 @@ describe('LanguageService', () => {
     let service: LanguageService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
         service = TestBed.inject(LanguageService);
     });
 
