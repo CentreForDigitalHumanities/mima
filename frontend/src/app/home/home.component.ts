01333e608c0b70909f9b3bec1c9575255921a9e7
Sheean Spoel <s.j.j.spoel@uu.nl>
2023-01-29 12:30:55 +0100

    Added linting

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BackendService } from './../services/backend.service';
 
 @Component({
-  selector: 'mima-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'mima-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
     constructor(private backend: BackendService) { }
-
-    ngOnInit(): void {
-    }
 
 }
