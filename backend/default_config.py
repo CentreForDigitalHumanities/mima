3ccc7ae391128b7d4f68c5d0087d51cc40dbe8ac
Meesch <meesstip@gmail.com>
2023-09-05 16:45:49 +0200

    add a default config for the backend

---
+++ backend/default_config.py
@@ -0,0 +1,8 @@
+## Path to the Meertens data in csv format
+DATA_PATH = ""
+
+## Path to the participants' data in csv format
+PARTICIPANTS_PATH = ""
+
+## Path to the output folder
+OUTPUT_PATH = ""
