cfcfa5b59e054a2c28574253dda272ed4bed75ad
Sheean Spoel <s.j.j.spoel@uu.nl>
2023-12-07 10:37:06 +0100

    Tweaked layout and search behavior

--- frontend/src/app/models/filter.ts
+++ frontend/src/app/models/filter.ts
@@ -6,6 +6,10 @@ export interface Filter {
     index: number;
     field: FilterField;
     content: string[];
+    /**
+     * Require the entire field to match this filter. This is needed when the text content could
+     * contain part of this filter. E.g. searching for "Fries" should not match "Westfries"
+     */
     onlyFullMatch: boolean;
 }
 
