7cce2c75cca3ad2c6e2cac5f7bc2ff73a99b3ae1
Sheean Spoel <s.j.j.spoel@uu.nl>
2025-01-16 10:42:20 +0100

    Updated dialect organization script

--- organize_dialects.py
+++ organize_dialects.py
@@ -3,21 +3,27 @@ from dataclasses import dataclass
 from typing import Dict, Iterable, List, Optional
 import re
 
+TRANSITION_MARKER = "Overgangsdialect"
+
 
 @dataclass
 class Dialect:
-    parent: Optional["Dialect"]
+    parents: List["Dialect"]
     name: str
 
     # number of levels
     def granularity(self) -> int:
-        if self.parent != None:
-            return self.parent.granularity() + 1
+        if self.parents:
+            return max(parent.granularity() + 1 for parent in self.parents)
         else:
             return 1
 
     def __str__(self) -> str:
-        return self.name if self.parent == None else f"{self.parent} > {self.name}"
+        return (
+            self.name
+            if not self.parents
+            else f"{','.join(str(parent) for parent in self.parents)} > {self.name}"
+        )
 
 
 # lookup from the end leaf of a dialect to a Dialect object
@@ -29,14 +35,14 @@ lookup: Dict[str, Dialect] = {}
 
 # check if the parent is a known (grand)parent of this dialect
 def validate_parent(child: Dialect, parent: Dialect) -> bool:
-    if parent == child.parent:
-        return True
-
-    if child.parent == None:
+    if not child.parents:
         return parent == None
 
+    if [parent] == child.parents:
+        return True
+
     # is it a parent of the dialect's parent?
-    return validate_parent(child.parent, parent)
+    return validate_parent(child.parents[0], parent)
 
 
 def parse_dialect(text: str) -> Dialect:
@@ -54,7 +60,7 @@ def parse_dialect(text: str) -> Dialect:
             if not validate_parent(dialect, parent):
                 # higher granularity?
                 print(
-                    f"Inconsistent parent for dialect {level};\nexpected '{dialect.parent}'\ngot '{parent}'.\nText={text}"
+                    f"Inconsistent parent for dialect {level};\nexpected '{dialect.parents[0]}'\ngot '{parent}'.\nText={text}"
                 )
                 granularity = 1 if parent == None else (parent.granularity() + 1)
                 existing_granularity = dialect.granularity()
@@ -71,7 +77,7 @@ def parse_dialect(text: str) -> Dialect:
                     #     f"Same or lower granularity {granularity} vs {existing_granularity}, ABORT!"
                     # )
         except KeyError:
-            dialect = Dialect(parent, level)
+            dialect = Dialect([parent] if parent else None, level)
             lookup[level] = dialect
         parent = dialect
 
@@ -92,14 +98,23 @@ with open("dialect_data.txt") as f:
     for line in f.readlines():
         parsed.append((line, list(parse_dialects(line))))
 
+Hierarchy = Dict[str, "Hierarchy"]
 
-def construct_hierarchy(dialects: Iterable[Dialect], parent: Optional[Dialect] = None):
-    tree = {}
+
+def construct_hierarchy(
+    dialects: Iterable[Dialect], parent: Optional[Dialect] = None
+) -> Hierarchy:
+    tree: Hierarchy = {}
     matched: List[Dialect] = []
     unmatched: List[Dialect] = []
 
     for dialect in dialects:
-        if dialect.parent == parent:
+        if (
+            not dialect.parents
+            and parent == None
+            or dialect.parents
+            and parent in dialect.parents
+        ):
             matched.append(dialect)
         else:
             unmatched.append(dialect)
@@ -108,6 +123,41 @@ def construct_hierarchy(dialects: Iterable[Dialect], parent: Optional[Dialect] =
         tree[dialect.name] = construct_hierarchy(unmatched, dialect)
 
     return tree
+
+
+def merge_children(hierarchy: Hierarchy, dialect: str, children: Hierarchy) -> None:
+    if dialect in hierarchy:
+        hierarchy[dialect] = {**hierarchy[dialect], **children}
+    else:
+        hierarchy[dialect] = children
+
+
+def transition_dialects(hierarchy: Hierarchy) -> Hierarchy:
+    """Splits transition dialects into two dialects; with the underlying children getting two parents
+
+    Args:
+        hierarchy (Hierarchy): hierarchy to parse the top nodes from
+
+    Returns:
+        Hierarchy: the updated hierarchy
+    """
+    updated: Hierarchy = {}
+
+    for name, children in hierarchy.items():
+        if name.startswith(TRANSITION_MARKER):
+            dialects = name.replace(TRANSITION_MARKER, "").strip().split("/")
+            for dialect in dialects:
+                merge_children(updated, dialect, children)
+
+            # update the Dialect data in the lookup
+            del lookup[name]
+            for child in children:
+                lookup[child].parents = [lookup[p] for p in dialects]
+        else:
+            # nothing to see here, move along
+            merge_children(updated, name, children)
+
+    return updated
 
 
 def print_hierarchy(tree: dict, indent: int = 0):
@@ -119,5 +169,8 @@ def print_hierarchy(tree: dict, indent: int = 0):
         print_hierarchy(tree[key], indent + 1)
 
 
+# Hierarchy contains the information using the dialect names (as string)
+# Use this name in the lookup to retrieve the Dialect object
 hierarchy = construct_hierarchy(lookup.values())
+hierarchy = transition_dialects(hierarchy)
 print_hierarchy(hierarchy)
