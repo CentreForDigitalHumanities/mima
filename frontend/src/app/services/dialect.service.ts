ac34b6a7712e2e2ff098e26e74342d546dca2bfb
Sheean Spoel <s.j.j.spoel@uu.nl>
2025-03-06 12:50:47 +0100

    Highlight matched dialects in judgments, fix ellipses

--- frontend/src/app/services/dialect.service.ts
+++ frontend/src/app/services/dialect.service.ts
@@ -2,6 +2,25 @@ import { Injectable } from '@angular/core';
 import { Dialect, DialectLookup, DialectPath, EndDialects } from '../models/dialect';
 import data from './dialect_hierarchy.json';
 import { Participant } from '../models/participant';
+import { MatchedParts } from '../models/matched-parts';
+
+export type MatchedSubItem<T, K extends keyof T> = {
+    // passthrough a selection of properties
+    [P in keyof Pick<T, K>]: MatchedParts
+} & {
+    participantId: MatchedParts,
+    dialects: MatchedParts[]
+};
+
+/**
+ * Matched sub items grouped by their text
+ */
+export type MatchedSubItemGrouped<K extends string> = {
+    [P in K]: MatchedParts
+} & {
+    text: string;
+    participantIds: MatchedParts[];
+};
 
 @Injectable({
     providedIn: 'root'
@@ -76,6 +95,25 @@ export class DialectService {
         return lookup.paths[dialect];
     }
 
+    initializeDialectTextParts() {
+        const dialectTextParts: { [dialect: string]: MatchedParts } = {};
+        for (const dialect of this.dialectLookup.flattened) {
+            dialectTextParts[dialect.name] = new MatchedParts({
+                empty: false,
+                emptyFilters: false,
+                fullMatch: false,
+                match: false,
+                parts: [{
+                    match: false,
+                    text: dialect.name,
+                    bold: false
+                }]
+            });
+        }
+
+        return dialectTextParts;
+    }
+
     /**
      * Determines for each participant which are the most
      * salient dialects (the dialect path) with the most steps.
@@ -108,5 +146,80 @@ export class DialectService {
         for (const child of this.dialectLookup.findChildren(dialect)) {
             yield child.name;
         }
+    }
+
+    groupSubItems<T extends MatchedSubItem<T, K>,
+        K extends keyof T & string,
+        U extends keyof T & string>(
+            matchedSubItems: T[],
+            keys: [K, ...U[]],
+            endDialects: EndDialects,
+            countDialectMatch?: (subItem: T, dialect: string) => void): [
+            { [dialect: string]: MatchedSubItemGrouped<K | U>[] },
+            { [dialect: string]: MatchedParts }
+        ] {
+        // group answers by their dialect
+        const matchedDialects: { [endDialects: string]: T[] } = {};
+        const matchedDialectParts: { [dialect: string]: MatchedParts } = {};
+
+        for (const subItem of matchedSubItems) {
+            // did the answer match the filter on dialect?
+            const subItemMatchedDialects: MatchedParts[] = [];
+            for (const parts of subItem.dialects) {
+                if (parts.match) {
+                    matchedDialectParts[parts.text] = parts;
+                    subItemMatchedDialects.push(parts);
+                }
+            }
+
+            for (const dialect of endDialects[subItem.participantId.text]) {
+                if ( // not matching on dialects: mark all the end dialects as matched
+                    subItemMatchedDialects.length == 0
+                    // only mark the dialects which were matched by the filters
+                    || (this.anyDialectInPaths(
+                        subItemMatchedDialects.map(x => x.text),
+                        this.getDialectPaths(dialect)))) {
+                    if (countDialectMatch) {
+                        countDialectMatch(subItem, dialect);
+                    }
+                    matchedDialects[dialect] = [...matchedDialects[dialect] ?? [], subItem];
+                }
+            }
+        }
+
+        // then group them by their text
+        const grouped: { [endDialects: string]: MatchedSubItemGrouped<K | U>[] } = {};
+
+        for (const [dialect, answers] of Object.entries(matchedDialects)) {
+            const dialectGroup: { [text: string]: T[] } = {};
+            for (const answer of answers) {
+                const text = answer[keys[0]].text;
+                if (text in dialectGroup) {
+                    dialectGroup[text].push(answer);
+                } else {
+                    dialectGroup[text] = [answer];
+                }
+            }
+
+            // sort by text; put unattested last
+            grouped[dialect] = [...Object.entries(dialectGroup)].sort(([textA], [textB]) =>
+                textA === ''
+                    ? 1
+                    : textB === ''
+                        ? -1
+                        : textA.localeCompare(textB)
+            ).map(([text, answers]) => {
+                const result: any = {};
+                for (const key of keys) {
+                    result[key] = answers[0][key];
+                };
+                result.text = text;
+                result.dialects = answers[0].dialects;
+                result.participantIds = answers.map(answer => answer.participantId);
+
+                return <MatchedSubItemGrouped<K | U>>result;
+            });
+        }
+        return [grouped, matchedDialectParts];
     }
 }
