4f6b99bc3aa4d21ce4d9d46333264353c3469945
Meesch <31687030+Meesch@users.noreply.github.com>
2025-09-10 16:45:21 +0200

    include chapters and tags in frisian likert

--- backend/read_data/read_likert_frisian.py
+++ backend/read_data/read_likert_frisian.py
@@ -1,4 +1,4 @@
-from mima.settings import FRISIAN_PATH, OUTPUT_PATH
+from mima.settings import FRISIAN_PATH, FRISIAN_META, OUTPUT_PATH
 import csv
 import os
 import json
@@ -9,11 +9,13 @@ from read_data.read_questionnaire_meertens import remove_periods
 def get_likert_items_and_indices(header):
     indices = []
     items = {}
+    index_to_id = {}
     for index, item in enumerate(header):
         if 'Invulzin' in item and '[' in item and ']' in item:
             indices.append(index)
             main_question = remove_periods(item.split('Invulzin: ')[1])
-            main_question_id = remove_periods(item.split('Invulzin: ')[1].split('[')[0])
+            main_question_id = remove_periods(item.split('Invulzin: ')[0]).strip()
+            index_to_id[index] = main_question_id
             sub_question = main_question.split('[')[1].split(']')[0]
             sub_question_id = main_question_id + "_" + sub_question
             main_question = main_question.replace('['+sub_question+']', "\u2026")
@@ -26,15 +28,15 @@ def get_likert_items_and_indices(header):
                 sub_question_id,
                 responses
             )
-            items[index] = judgment_item
+            items[main_question_id] = judgment_item
 
-    return indices, items
+    return indices, items, index_to_id
 
-def get_likert_responses(line, indices, judgment_items):
+def get_likert_responses(line, indices, index_to_id, judgment_items):
     participant_id = line[0]
     for index in indices:
         score = line[index]
-        judgment_items[index].responses.append(
+        judgment_items[index_to_id[index]].responses.append(
             Response(
                 participant_id,
                 ['Frisian', 'Klaaifrysk'],
@@ -50,9 +52,20 @@ with open(FRISIAN_PATH, encoding="utf8") as file:
     for line in reader:
         frisian_data.append(line)
 
-likert_indices, judgment_items = get_likert_items_and_indices(frisian_data[0])
+likert_indices, judgment_items, index_to_id = get_likert_items_and_indices(frisian_data[0])
 for line in frisian_data[1:]:
-    judgment_items = get_likert_responses(line, likert_indices, judgment_items)
+    judgment_items = get_likert_responses(line, likert_indices, index_to_id, judgment_items)
+
+with open(FRISIAN_META, encoding='utf8') as file:
+    reader = csv.reader(file)
+    for line in reader:
+        question_id = line[0]
+        if question_id in judgment_items.keys():
+            judgment_items[question_id].chapters = line[2].split(';')
+            judgment_items[question_id].tags = line[3].split(';')
+            judgment_items[question_id].translation = line[4]
+            judgment_items[question_id].gloss = line[5]
+
 
 with open(os.path.join(OUTPUT_PATH, "likert_scales_frisian.json"), "w") as file:
         json.dump(judgment_items, file, default=serialize_classes, indent=4)
