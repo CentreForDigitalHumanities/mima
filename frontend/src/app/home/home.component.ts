eae00b9ffcaad7e818024b66a6741126fe11a3d6
Meesch <31687030+Meesch@users.noreply.github.com>
2025-09-24 22:14:45 +0200

    update home page with new copy

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { BackendService } from './../services/backend.service';
+import { IsActiveMatchOptions, RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
     selector: 'mima-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss'],
+    imports: [RouterLink, RouterLinkActive],
+    standalone: true
 })
 export class HomeComponent {
     constructor(private backend: BackendService) { }
