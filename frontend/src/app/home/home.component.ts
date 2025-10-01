a3c09ef9c6c3a94c658283b9b36b6209650f049c
Sheean Spoel <s.j.j.spoel@uu.nl>
2025-10-01 16:04:00 +0200

    I can haz token count?

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,14 +1,31 @@
+import { formatNumber } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { tokenCount } from '../../environments/counts';
 
 @Component({
     selector: 'mima-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    imports: [RouterLink, RouterLinkActive],
+    imports: [FontAwesomeModule, RouterLink, RouterLinkActive],
     standalone: true
 })
 export class HomeComponent {
-    constructor() { }
+    date: string;
+    tokenCount = formatNumber(tokenCount, $localize`en-GB`);
+    faLink = faLink;
+
+    constructor() {
+        const now = new Date();
+        this.date = now.toLocaleString(
+            $localize`en-GB`,
+            {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            });
+    }
 
 }
